78e51b808c33258c75ccb058c83ccebe2ef53a88
Patrick <kelly.patj@gmail.com>
2022-12-08 14:46:42 -0500

    setup init function

--- js/app.js
+++ js/app.js
@@ -3,11 +3,12 @@
 
 
 /*---------------------------- Variables (state) ----------------------------*/
-
+let board, turn, winner, tie;
 
 
 /*------------------------ Cached Element References ------------------------*/
-
+const squareEls = document.getElementsByClassName('sqr');
+const msgEl = document.getElementById('message');
 
 
 /*----------------------------- Event Listeners -----------------------------*/
@@ -15,4 +16,19 @@
 
 
 /*-------------------------------- Functions --------------------------------*/
+document.onload = init();
 
+function init(){
+  // console.log(squareEls)
+  board = [];
+  turn = -1;
+  winner = false;
+  tie = false;
+
+  for (const sqr of squareEls) board.push(null);
+  render();
+}
+
+function render(){
+  
+}
